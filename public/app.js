a2adf28aff7f209d832b1d5f8fadba37c0595022
Aurisfy CEO <aurisfy@gmail.com>
2023-09-10 09:21:51 -0400

    user able to save and see favorites

--- public/app.js
+++ public/app.js
@@ -1,33 +1,21 @@
+const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
 
-const favorites = JSON.parse(localStorage.getItem('favorites') || '[]')
-
-
-
-function addToFav(src, id)
-{
-     
-    favorites.push({src, id})
-    localStorage.setItem('favorites', JSON.stringify(favorites))
-
-
+function addToFav(src, id) {
+  favorites.push({ src, id });
+  localStorage.setItem("favorites", JSON.stringify(favorites));
 }
 
-
-function shareWithFriends(id)
-{
-   
-    if (navigator.share && navigator.canShare) {
-       navigator.share( {
-        title : 'Free Photos For Everyone on Snapz',
-        url : `/photo/${id}`
-       })
-    } else {
-        alert('web share api not supported link has been copied to your clipboard')
-    }
+function shareWithFriends(id) {
+  if (navigator.share && navigator.canShare) {
+    navigator.share({
+      title: "Free Photos For Everyone on Snapz",
+      url: `/photo/${id}`,
+    });
+  } else {
+    alert("web share api not supported link has been copied to your clipboard");
+  }
 }
 
-
-function openInFull(id)
-{
-    location.href = `/photo/${id}`
+function openInFull(id) {
+  location.href = `/photo/${id}`;
 }
--- server.js
+++ server.js
@@ -1,33 +1,29 @@
-
-const { error } = require('console');
-const express = require('express');
+const { error } = require("console");
+const express = require("express");
 const server = express();
-const axios = require('axios')
+const axios = require("axios");
 
+const unsplashApiKey = "Ryd5V1rN8vRB2L8d8BW8A5bEcOtq0mWVZF-GAf1wuJw";
 
-const unsplashApiKey = 'Ryd5V1rN8vRB2L8d8BW8A5bEcOtq0mWVZF-GAf1wuJw'
-
-
-const Port = process.env.Port || 3000
+const Port = process.env.Port || 3000;
 
 //***** setting Middlewars */
 
-server.use(express.static('public'));
-server.set('view engine', 'ejs')
- 
-  
+server.use(express.static("public"));
+server.set("view engine", "ejs");
 
 //***** Setting Endpoints */
 
-
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   const page = req.query.page;
   const query = req.query.q;
 
   // Define the API URL based on whether a query is provided
   let apiUrl;
   if (query) {
-    apiUrl = `https://api.unsplash.com/search/photos/?page=${page || 1}&per_page=30&query=${query}&client_id=${unsplashApiKey}`;
+    apiUrl = `https://api.unsplash.com/search/photos/?page=${
+      page || 1
+    }&per_page=30&query=${query}&client_id=${unsplashApiKey}`;
   } else {
     apiUrl = `https://api.unsplash.com/photos/random?count=20&client_id=${unsplashApiKey}`;
   }
@@ -39,68 +35,59 @@ server.get('/', (req, res) => {
       const data = response.data.results || response.data; // Use results for search and data for random
 
       // Render the 'index' template with the fetched data
-      res.render('index', { data });
+      res.render("index", { data });
     })
     .catch(function (error) {
       console.log(error);
-      res.status(500).send('Error fetching photo data');
+      res.status(500).send("Error fetching photo data");
     });
 });
 
 
+server.get('/favorites', (req, res) => {
+
+  res.render('favorites');
+
+})
 
 
-
-
-
-
-server.get('/photos/:q', (req, res) => {
-  
-  const query = req.params.q
- //console.log(query)
- // Construct the URL for the Unsplash API
+server.get("/photos/:q", (req, res) => {
+  const query = req.params.q;
+  //console.log(query)
+  // Construct the URL for the Unsplash API
   const apiUrl = `https://api.unsplash.com/search/photos/?page=1&per_page=30&query=${query}&client_id=${unsplashApiKey}`;
-  
-   
+
   axios
-  .get(apiUrl)
-  .then(function (images) {
-    const data = images.data.results // Get the data from the response
-     res.render('index', { data })
-   
-  })
-  .catch(function (error) {
-    console.error(error);
-    res.status(500).send('Error fetching photo data');
-  });
+    .get(apiUrl)
+    .then(function (images) {
+      const data = images.data.results; // Get the data from the response
+      res.render("index", { data });
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error fetching photo data");
+    });
 });
 
-server.get('/photo/:id', (req, res) => {
-
-
-  const id = req.params.id
+server.get("/photo/:id", (req, res) => {
+  const id = req.params.id;
   // Construct the URL for the Unsplash API
   const apiUrl = `https://api.unsplash.com/photos/${id}?client_id=${unsplashApiKey}`;
-  
-   
+
   axios
-  .get(apiUrl)
-  .then(function (response) {
-    const data = response.data // Get the data from the response
-     res.render('template', { data })
-  })
-  .catch(function (error) {
-    console.error(error);
-    res.status(500).send('Error fetching photo data');
-  });
+    .get(apiUrl)
+    .then(function (response) {
+      const data = response.data; // Get the data from the response
+      res.render("template", { data });
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error fetching photo data");
+    });
 });
-
-
-
-
 
 //***** Setting a Port for my server */
 
 server.listen(Port, () => {
-    console.log(`Server is running on Port : ${Port}`)
-})
+  console.log(`Server is running on Port : ${Port}`);
+});
